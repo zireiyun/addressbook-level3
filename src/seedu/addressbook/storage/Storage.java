cd1c2ec41acdf10270447fc680a03ddd5ee9f246
zireiyun <zirei0303@hotmail.com>
2017-02-27 22:44:18 +0800

    Add abstract class Storage

---
+++ src/seedu/addressbook/storage/Storage.java
@@ -0,0 +1,34 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.storage.Storage.StorageOperationException;
+
+public abstract class Storage {
+   
+    /**
+     * Signals that the given file path does not fulfill the storage filepath constraints.
+     */
+    public static class InvalidStorageFilePathException extends IllegalValueException {
+        public InvalidStorageFilePathException(String message) {
+            super(message);
+        }
+    }
+    
+    /**
+     * Signals that some error has occured while trying to convert and read/write data between the application
+     * and the storage file.
+     */
+    public static class StorageOperationException extends Exception {
+        public StorageOperationException(String message) {
+            super(message);
+        }
+    }
+    
+    public abstract void save(AddressBook addressBook) throws StorageOperationException;
+    
+    public abstract AddressBook load() throws StorageOperationException;
+    
+    public abstract String getPath();
+ 
+}
